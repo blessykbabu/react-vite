b6c269592056dd9c7c5bbc323af0d5b3b647262e
blessykbabu <blessykbabu5@gmail.com>
2023-11-08 11:31:12 +0530

    formik

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,17 +1,20 @@
 import { useState } from 'react'
+
 import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 // import './App.css'
 import UncontrolledInput from './components/Uncontrolledinput'
 import ControlledInput from './components/Controlledinput'
+import Basic from './components/Basic'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <ControlledInput/>
-     <UncontrolledInput/>
+    {/* <ControlledInput/>
+     <UncontrolledInput/> */}
+     <Basic/>
     </>
   )
 }
